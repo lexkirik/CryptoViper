9dd87905a952477e834cce12e0f44891babd5027
Aliaksei Kirykovich <lexkirik@mail.ru>
2024-01-17 21:10:59 +0300

    first commit

---
+++ CryptoViper/Entity.swift
@@ -0,0 +1,13 @@
+//
+//  Entity.swift
+//  CryptoViper
+//
+//  Created by Test on 18.12.23.
+//
+
+import Foundation
+
+struct Crypto: Decodable {
+    let currency: String
+    let price: String
+}
